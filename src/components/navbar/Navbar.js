83542582a3e57007da664452e4d48eceb83bd5b4
Docenko Pasha <pashadocenko@gmail.com>
2023-07-23 21:55:14 +0300

    init Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import "./Navbar.css"
 import {NavLink} from "react-router-dom";
+import Logo from "../../utils/img/logo.svg";
 
 function Navbar({items}) {
     return (
-        <div className="menu_header">
-            <div className="nav_links">
-                <NavLink to={"/"}>Главная</NavLink>
+        <nav className="menu">
+            <div className="menu_logo">
+                <img src={Logo} alt="Help not to die"/>
             </div>
-            <div className="nav_links">
-                <NavLink to={"/dtpWillHappened"}>ДТП что делать?</NavLink>
+            <div className="menu_header_links_with_text">
+                <div className="menu_header_links">
+                    <NavLink className="menu_header_link" to={"/"}>Главная</NavLink>
+                    <NavLink className="menu_header_link" to={"/dtpWillHappened"}>ДТП, что делать?</NavLink>
+                    <NavLink className="menu_header_link" to={"/dtpWithAnimals"}>ДТП с животными</NavLink>
+                    <NavLink className="menu_header_link" to={"/medicalSupport"}>Медицинская поомщь при ДТП</NavLink>
+                    <NavLink className="menu_header_link" to={"/contacts"}>Контакты</NavLink>
+                </div>
+                <div className="menu_header_text">
+                    <p className="menu_header_text_first">Help And Not Die</p>
+                    <p className="menu_header_text_second">Помочь и не погибнуть</p>
+                </div>
             </div>
-            <div className="nav_links">
-                <NavLink to={"/dtpWithAnimals"}>ДТП с животными</NavLink>
-            </div>
-            <div className="nav_links">
-                <NavLink to={"/medicalSupport"}>Медицинская поомщь при ДТП</NavLink>
-            </div>
-            <div className="nav_links">
-                <NavLink to={"/contacts"}>Контакты</NavLink>
-            </div>
-        </div>
+        </nav>
     );
 }
 
