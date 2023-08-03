2517066751429d2fe5a1ac3f35b4604e530abc41
Docenko Pasha <pashadocenko@gmail.com>
2023-08-03 22:22:30 +0300

    add style in MedicalSupport.js

--- src/components/medicalSupport/BlockForMedicalSupport/BlockForMedicalSupport.js
+++ src/components/medicalSupport/BlockForMedicalSupport/BlockForMedicalSupport.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import "./BlockForMedicalSupport.css"
 
 function BlockForMedicalSupport({data}) {
     return (
         <div>
 
             {data.map(item => (
-                <div key={item.id}>
-                    <div>
-                        <img src={item.img} alt=""/>
+                <div key={item.id} className="BlockForMedicalSupport">
+                    <div className="BlockForMedicalSupport_imgs">
+                        {/*<img src={item.img} alt=""/>*/}
+                        {
+                            item.img.map( item =>
+                                <img key={item}  src={item} alt="BlockForMedicalSupport_imgs"/>
+                            )
+                        }
                     </div>
 
-                    <div>
+                    <div className="BlockForMedicalSupport_text">
                         <h3>{item.title}</h3>
-                        <p>{item.description}</p>
+                        {
+                            item.description.map( item =>
+                                <p key={item}>{item}</p>
+                            )
+                        }
                     </div>
                 </div>
             ))}
