1c3c8d8a0d78612da2644f13923bfc8ae8dc8203
Docenko Pasha <pashadocenko@gmail.com>
2023-08-04 23:18:24 +0300

    start to init Contacts.js

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import "./Contact.css"
+import telegramm from "../../utils/img/imgs_main_page/f4/telegramm.png";
+import viber from "../../utils/img/imgs_main_page/f4/viber.png";
+import watsapp from "../../utils/img/imgs_main_page/f4/watsapp.png";
+import img from "../../utils/img/imgs_main_page/f1/img.png";
 function Contacts() {
     return (
         <div>
@@ -11,27 +15,56 @@ function Contacts() {
                         <p><span>211440 г. Новополоцк</span></p>
                     </div>
                     <div className="first_section_contact_block">
-                        <div>
+                        <div className="first_section_contact_block_block">
                             <p>Телефон</p>
                             <p><span>+375 29 758 50 06</span></p>
                         </div>
-                        <div>
+                        <div className="first_section_contact_block_block">
                             <p>Viber</p>
                             <p><span>+375 29 656 14 22</span></p>
                         </div>
                     </div>
                     <div className="first_section_contact_block">
-                        <div>
+                        <div className="first_section_contact_block_block">
                             <p>Email</p>
                             <a href="mailto:AlehBashun@gmail.com"><span>AlehBashun@gmail.com</span></a>
                         </div>
-                        <div>
+                        <div className="first_section_contact_block_block">
                             <p>Буду рад Вашим предложениям и замечаниям по проекту</p>
                         </div>
                     </div>
                 </div>
             </section>
-            <section>
+            <section className="second_section_contact">
+                <div className="containerContact">
+                    <div className="second_section_contact_with_foto"></div>
+                    <div>
+                        <h2>Цель проект <span>HAND:</span></h2>
+                        <p>Формирование, на основе виртуального опыта и переживаний, знаний и навыков направленных на обдуманное, грамотное и предсказуемое поведение и действия при чрезвычайных проишествиях и ДТП.</p>
+                    </div>
+                    <div>
+                        <div className="">
+                            <p>Автор проекта</p>
+                            <p>«Help And Not Die»</p>
+                            <h2>Олег</h2>
+                            <h2>Башун</h2>
+                        </div>
+                        <div className="">
+                            <p>Отвечаю онлайн</p>
+                            <div>
+                                <a href="*">
+                                    <img src={telegramm} alt="telegramm"/>
+                                </a>
+                                <a href="*">
+                                    <img src={watsapp} alt="watsapp"/>
+                                </a>
+                                <a href="*">
+                                    <img src={viber} alt="viber"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </section>
         </div>
