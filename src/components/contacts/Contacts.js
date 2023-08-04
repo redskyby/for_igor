016f968688fd7fe6ce23ef4fb2db007ce65c1469
Docenko Pasha <pashadocenko@gmail.com>
2023-08-04 22:51:22 +0300

    start to init Contacts.js

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,29 +1,29 @@
 import React from 'react';
-
+import "./Contact.css"
 function Contacts() {
     return (
         <div>
-            <section>
+            <section className="containerContact">
                 <h1>Контакты</h1>
-                <div>
-                    <div>
+                <div className="first_section_contact">
+                    <div className="first_section_contact_block">
                         <p>Адрес</p>
-                        <p>211440 г. Новополоцк</p>
+                        <p><span>211440 г. Новополоцк</span></p>
                     </div>
-                    <div>
+                    <div className="first_section_contact_block">
                         <div>
                             <p>Телефон</p>
-                            <p>+375 29 758 50 06</p>
+                            <p><span>+375 29 758 50 06</span></p>
                         </div>
                         <div>
                             <p>Viber</p>
-                            <p>+375 29 656 14 22</p>
+                            <p><span>+375 29 656 14 22</span></p>
                         </div>
                     </div>
-                    <div>
+                    <div className="first_section_contact_block">
                         <div>
                             <p>Email</p>
-                            <a href="mailto:AlehBashun@gmail.com">AlehBashun@gmail.com</a>
+                            <a href="mailto:AlehBashun@gmail.com"><span>AlehBashun@gmail.com</span></a>
                         </div>
                         <div>
                             <p>Буду рад Вашим предложениям и замечаниям по проекту</p>
