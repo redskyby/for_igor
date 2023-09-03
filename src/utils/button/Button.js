a222d4e27814751403b47cdeaa22d307820b4f48
Docenko Pasha <pashadocenko@gmail.com>
2023-09-03 14:08:58 +0300

    Download link was tested.

--- src/utils/button/Button.js
+++ src/utils/button/Button.js
@@ -2,11 +2,22 @@ import React from 'react';
 import "./Button.css";
 
 function Button(props) {
+    const downloadFile = () => {
+        const link = document.createElement('a');
+        link.href = 'https://ru.files.fm/u/fbtc34tn'; // замените на ссылку на файл
+        link.setAttribute('download', 'file.exe');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <button
+            onClick={downloadFile}
             className="button"
             type={props.type}
             placeholder={props.placeholder}>{props.name}</button>
+
     );
 }
 
